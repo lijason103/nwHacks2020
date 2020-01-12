e9d77537bddd1a32e63e60af6b178e3cd158a12a
lijason103 <lijason103@gmail.com>
2020-01-11 19:37:41 -0800

    Added jobs post

--- server/index.js
+++ server/index.js
@@ -1,6 +1,9 @@
 const express = require('express')
+const bodyParser = require('body-parser')
+
 const app = express()
-const port = 3000
+app.use(bodyParser.json())
+const port = 5000
 
 const standard_lib_token = 'tok_dev_eVoWNiq7rJ7u7H1vwYRa78ZytVfDWmrHhfS98NkWvmEag7qGgTbozEWUujtevB6T'
 const lib = require('lib')({token: standard_lib_token});
@@ -8,7 +11,7 @@ const query = lib.googlesheets.query['@0.3.0'];
 
 app.post('/login', (req, res) => {
     const user_id = 'hi'
-    res.send({user_id})
+    res.send({ user_id })
 })
 
 app.get('/jobs', (req, res) => {
@@ -19,9 +22,33 @@ app.get('/jobs', (req, res) => {
         let result = await query.select({
             range: "jobs!A1:G999" // (required)
         });
-        res.send(result)
+        console.log(result)
+        res.send(result.rows)
     }
     getJobs()
+})
+
+app.post('/jobs', (req, res) => {
+    const { user_id, url, selector, condition, value } = req.body
+    const id = 100
+
+    const addJob = async () => {
+        await query.insert({
+            range: "jobs!A1:G4",
+            fieldsets: [
+              {
+                "id": id,
+                "user_id": user_id,
+                "url": url,
+                "selector": selector,
+                "condition": condition,
+                "value": value,
+              }
+            ]
+        });
+        res.sendStatus(200)
+    }
+    addJob()
 })
 
 app.post('/send-sms', (req, res) => {
