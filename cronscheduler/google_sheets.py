89dbb3efc2a7bc4a8f1ab86a996c1d1e349a03fb
Vincent Wong <vwong125@my.bcit.ca>
2020-01-12 10:57:13 -0800

    added logic for num

--- cronscheduler/google_sheets.py
+++ cronscheduler/google_sheets.py
@@ -1,7 +1,7 @@
 import requests
 
 def get_data():
-    r = requests.get('http://localhost:5000/all-jobs')
+    r = requests.get('http://localhost:8080/all-jobs')
     return r.json()
 
 if __name__ == '__main__':
